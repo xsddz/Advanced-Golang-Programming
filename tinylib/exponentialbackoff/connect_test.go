2c2d217cc201decd804d716028b1b41556a26c00
xsddz <dingzhns@163.com>
2021-04-01 16:46:30 +0800

    add exponential backoff algorithm

---
+++ tinylib/exponentialbackoff/connect_test.go
@@ -0,0 +1,35 @@
+package exponentialbackoff_test
+
+import (
+	"Advanced-Golang-Programming/tinylib/exponentialbackoff"
+	"fmt"
+	"testing"
+)
+
+// TestConnectRetry -
+// run:
+//     go test -v -run="TestConnectRetry"
+func TestConnectRetry(t *testing.T) {
+	type args struct {
+		network string
+		address string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "case 1",
+			args: args{
+				network: "tcp",
+				address: "192.168.0.1:8078",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conn, err := exponentialbackoff.ConnectRetry(tt.args.network, tt.args.address)
+			fmt.Println("conn:", conn, err)
+		})
+	}
+}
