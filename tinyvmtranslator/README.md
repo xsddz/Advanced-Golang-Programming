6ce5f5abbe0c9db782c97a35fca3520d93cab423
xsddz <dingzhns@163.com>
2021-04-14 17:51:44 +0800

    add tinyvmtranslator

---
+++ tinyvmtranslator/README.md
@@ -0,0 +1,14 @@
+
+### 编译
+
+```
+$ go build -o tinyvmtranslator_mac cmd/main.go
+```
+
+### 使用
+
+如下，将会`source`目录下的多个`.vm`文件编译为`[path/]filename.hack`：
+
+```
+Usage: tinyassembler [path/]filename.asm
+```
---
+++ tinyvmtranslator/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: tinyassembler filename.asm\n\n")
+		return
+	}
+
+	targetFileDir := filepath.Dir(os.Args[1])
+	sourceFile := filepath.Base(os.Args[1])
+	targetFile := strings.TrimSuffix(sourceFile, filepath.Ext(sourceFile)) + ".hack"
+
+}
