1184e4083f42a54ce80421b1b744b71ea79bccd4
xsddz <dingzhns@163.com>
2021-04-22 14:24:40 +0800

    add rwb

--- ex/main.go
+++ ex/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"Advanced-Golang-Programming/ex/subcollect"
 	"fmt"
 )
 
@@ -10,23 +9,4 @@ func init() {
 }
 
 func main() {
-	// theBugs.Run()
-	// kh.Run()
-
-	subcollect.Run()
-
-	// fmt.Println("123 + 0 = ", foobar.BigAdder("123", "0"))
-	// fmt.Println("123 + 2 = ", foobar.BigAdder("123", "2"))
-	// fmt.Println("123 + 22 = ", foobar.BigAdder("123", "22"))
-	// fmt.Println("123 + 223 = ", foobar.BigAdder("123", "223"))
-	// fmt.Println("123 + 2234 = ", foobar.BigAdder("123", "2234"))
-
-	// fmt.Println(foobar.BigMult("12345", "23"))
-	// fmt.Println(foobar.BigMult("23", "12345"))
-
-	// fmt.Println(foobar.BigMult("12345", "0"))
-	// fmt.Println(foobar.BigMult("0", "12345"))
-
-	// fmt.Println(foobar.BigMult("12345", "1"))
-	// fmt.Println(foobar.BigMult("1", "12345"))
 }
